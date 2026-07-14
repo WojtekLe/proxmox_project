5ffe8518c7cb5eb1587e91be1802e9b2b08ad0aa
w.leligdon <wojciech.leligdon@gmail.com>
2026-07-14 10:05:26 +0200

    update terraform files

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "template" {
+  type = string
+}
+
+variable "cpu" {
+  type = number
+}
+
+variable "memory" {
+  type = number
+}
+
+variable "disk" {
+  type = number
+}
