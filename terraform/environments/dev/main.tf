2e727ed887b3b4b0448651d70ffc0e9132037627
WojtekLe <63649748+WojtekLe@users.noreply.github.com>
2026-07-14 13:01:49 +0200

    Devops pipeline (#2)
    * init 2
    
    * docs
    
    * decision
    
    * update 2
    
    * add picture
    
    * validator
    
    * catalog
    
    * manifest
    
    * tests
    
    * validator
    
    * validator start
    
    * feat(validator): implement YAML request loader
    
    * validation successful
    
    * validator ok
    
    * generate tfvars
    
    * update terraform files
    
    * test connection
    
    * fixing
    
    ---------
    
    Co-authored-by: w.leligdon <wojciech.leligdon@gmail.com>

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,17 +1,42 @@
 
-resource "proxmox_virtual_environment_vm" "vm" {
-
-  name = var.project
-
-  cpu {
-    cores = var.cpu
-  }
-
-  memory {
-    dedicated = var.memory
-  }
-
-  disk {
-    size = var.disk
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "~> 0.84"
+    }
   }
 }
+
+
+provider "proxmox" {
+  endpoint = "https://pve:8006/api2/json"
+
+  api_token = "${var.proxmox_token_id}=${var.proxmox_token_secret}"
+
+  insecure = true
+}
+
+data "proxmox_virtual_environment_nodes" "nodes" {}
+
+output "nodes" {
+  value = data.proxmox_virtual_environment_nodes.nodes.names
+}
+
+
+# resource "proxmox_virtual_environment_vm" "vm" {
+
+#   name = var.project
+
+#   cpu {
+#     cores = var.cpu
+#   }
+
+#   memory {
+#     dedicated = var.memory
+#   }
+
+#   disk {
+#     size = var.disk
+#   }
+# }
