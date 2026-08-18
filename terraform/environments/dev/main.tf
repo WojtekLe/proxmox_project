0e20db93db8bd1ee7be2ea77ec2bb2c2aa298f2b
w.leligdon <wojciech.leligdon@gmail.com>
2026-08-18 16:32:24 +0200

    added to description

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,10 +5,10 @@ resource "proxmox_virtual_environment_vm" "vm" {
     for index, vm in var.vms : index => vm
   }
 
-  name      = "${each.value.project}-${each.key + 1}"
+  name      = "${each.value.project}-${each.key}"
   node_name = "pve"
 
-  description = "Project ${each.value.project} with template ${each.value.template}"
+  description = "Project ${each.value.project} with template ${each.value.template}. Owner: ${each.value.owner}. Clone: ${each.value.vm_id}. CPU: ${each.value.cpu}. Memory: ${each.value.memory}. Disk: ${each.value.disk}."
 
   clone {
     vm_id = each.value.vm_id
